514a3df3b53874279cc92b3379a61db1950ec438
anuchouhan <113912497+anuchouhan@users.noreply.github.com>
2024-08-28 20:23:19 +0530

    Add files via upload

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = 'b32c17eed0956c03364e7843aa39cc55';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
